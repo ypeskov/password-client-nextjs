fb2137581dc86b01658d02d86228876cbd31449f
Yuriy Peskov <yuriy.peskov@gmail.com>
2022-05-04 09:30:04 +0300

    draft register

--- pages/register-user.tsx
+++ pages/register-user.tsx
@@ -1,32 +1,62 @@
-import type { NextPage } from 'next';
+import type {NextPage} from 'next';
 import Link from "next/link";
+import {FormEvent, MutableRefObject, useRef} from "react";
 
 const RegisterUserPage: NextPage = () => {
+  const emailRef: any = useRef<HTMLInputElement>();
+  const firstNameRef: any = useRef<HTMLInputElement>();
+  const lastNameRef: any = useRef<HTMLInputElement>();
+  const passwordRef: any = useRef<HTMLInputElement>();
+
+  const submitHandler = async (event: FormEvent) => {
+    event.preventDefault();
+
+    const email: string = emailRef.current.value;
+    const firstName: string = firstNameRef.current.value;
+    const lastName:string = lastNameRef.current.value;
+    const password: string = passwordRef.current.value;
+
+    const response = await fetch('http://localhost:8000/auth/user', {
+      method: 'POST',
+      body: JSON.stringify({
+        email,
+        password,
+        first_name: firstName,
+        last_name: lastName
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+    const data = await response.json();
+    console.log(data);
+  };
+
   return (
     <>
       <div className="container">
         <div className="row">
           <div className="col">
-            <form action="http://localhost:8000/auth/login" method="POST">
+            <form>
               <div className="mb-3">
                 <label htmlFor="email" className="form-label">Email address</label>
-                <input type="email" className="form-control" id="email" aria-describedby="emailHelp"/>
+                <input type="email" className="form-control" id="email" aria-describedby="emailHelp" ref={emailRef}/>
                 <div id="emailHelp" className="form-text">Email address is used for login</div>
               </div>
 
               <div className="mb-3">
                 <label htmlFor="first-name" className="form-label">First Name</label>
-                <input type="text" className="form-control" id="first-name"/>
+                <input type="text" className="form-control" id="first-name" ref={firstNameRef} />
               </div>
 
               <div className="mb-3">
                 <label htmlFor="last-name" className="form-label">Last Name</label>
-                <input type="text" className="form-control" id="last-name"/>
+                <input type="text" className="form-control" id="last-name" ref={lastNameRef}/>
               </div>
 
               <div className="mb-3">
                 <label htmlFor="password" className="form-label">Password</label>
-                <input type="password" className="form-control" id="password"/>
+                <input type="password" className="form-control" id="password" ref={passwordRef} />
               </div>
 
               <div className="mb-3">
@@ -34,7 +64,7 @@ const RegisterUserPage: NextPage = () => {
                 <input type="password" className="form-control" id="password-repeat"/>
               </div>
 
-              <button type="submit" className="btn btn-primary">Login</button>
+              <button type="submit" className="btn btn-primary" onClick={submitHandler}>Register</button>
             </form>
           </div>
 
